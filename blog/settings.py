349fc38a7887eb7a3b2af5a747f0daecdfe4d254
isa1989 <isa@starex.az>
2022-07-29 03:02:32 +0400

    update appc

--- blog/settings.py
+++ blog/settings.py
@@ -16,6 +16,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+import django_heroku
+import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
